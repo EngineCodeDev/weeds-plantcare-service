94552a9ad751f6ca5b4731a00e64450669189a13
tom-zoladz <tomasz.d.zoladz@gmail.com>
2023-08-27 22:26:34 +0200

    Spring Boot project initialized #2

---
+++ src/test/java/com/example/weeds/plantcareservice/WeedsPlantcareServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weeds.plantcareservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeedsPlantcareServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
