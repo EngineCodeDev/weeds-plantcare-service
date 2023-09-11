5b91bc73155047425af82c4e497d052a0a48b0a1
tom-zoladz <tomasz.d.zoladz@gmail.com>
2023-09-11 22:15:22 +0200

    user-init.sql modified

--- database/src/main/resources/init/user-init.sql
+++ database/src/main/resources/init/user-init.sql
@@ -1,10 +1,13 @@
 CREATE DATABASE weeds_db;
 CREATE USER weeds_admin WITH PASSWORD 'weeds_pass123';
+ALTER USER weeds_admin CREATEROLE;
 GRANT CONNECT ON DATABASE weeds_db TO weeds_admin;
+GRANT CREATE ON DATABASE weeds_db TO weeds_admin;
 -- change database to weeds_db
-GRANT USAGE, CREATE ON SCHEMA public TO weeds_admin;
+CREATE SCHEMA app;
+GRANT USAGE, CREATE ON SCHEMA app TO weeds_admin;
 
 CREATE USER weeds_user WITH PASSWORD 'weeds_pass123';
 GRANT CONNECT ON DATABASE weeds_db TO weeds_user;
 -- change database to weeds_db
-GRANT USAGE ON SCHEMA public TO weeds_user;
+GRANT USAGE ON SCHEMA app TO weeds_user;
