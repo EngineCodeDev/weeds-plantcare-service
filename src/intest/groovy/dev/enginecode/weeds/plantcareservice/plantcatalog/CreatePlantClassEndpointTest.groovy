3bb99fce11a8d60c79ed8ec79b0f62c5aef6e7b6
tom-zoladz <tomasz.d.zoladz@gmail.com>
2023-11-11 10:24:04 +0100

    CreatePlantClassEndpointTest added

---
+++ src/intest/groovy/dev/enginecode/weeds/plantcareservice/plantcatalog/CreatePlantClassEndpointTest.groovy
@@ -0,0 +1,57 @@
+package dev.enginecode.weeds.plantcareservice.plantcatalog
+
+import dev.enginecode.weeds.plantcareservice.intestspec.IntestSpecification
+import org.skyscreamer.jsonassert.JSONAssert
+import org.skyscreamer.jsonassert.JSONCompareMode
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+
+class CreatePlantClassEndpointTest extends IntestSpecification {
+
+    private static final def CREATE_PLANT_CLASS_PAYLOAD = """
+        {
+          "groups": ["trees"],
+          "entries": [
+            {"key": "name", "value": "Oak", "type": "string", "info": "Very popular tree"},
+            {"key": "average age", "value": "300", "type": "string", "info": "It's quite a lot"}
+          ]
+        }
+      """
+
+    private static final def EXPECTED_PLANT_CLASS_VIEW_JSON = """
+        {
+          "items": [
+            {
+              "entries": [
+                  {"key": "name", "value": "Oak", "type": "string", "info": "Very popular tree"},
+                  {"key": "average age", "value": "300", "type": "string", "info": "It's quite a lot"}
+                ]
+            }
+          ]
+        }
+      """
+
+    def "should save plant class to database"() {
+        when:
+        def response = testRestTemplate.exchange(
+                "http://localhost:$port/plantcare-service/plant-classes",
+                HttpMethod.POST,
+                getRequestEntity(CREATE_PLANT_CLASS_PAYLOAD),
+                String.class
+        )
+
+        then:
+        response.statusCode == HttpStatus.OK
+
+        when:
+        def responseEntity = testRestTemplate.getForEntity(
+                "http://localhost:$port/plantcare-service/plant-classes",
+                String.class
+        )
+
+        then:
+        JSONAssert.assertEquals(EXPECTED_PLANT_CLASS_VIEW_JSON, responseEntity.body, JSONCompareMode.LENIENT)
+    }
+
+}
+
