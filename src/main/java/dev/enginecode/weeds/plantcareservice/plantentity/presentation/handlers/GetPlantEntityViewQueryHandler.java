0c4bfda16e5c68bcedc4f4796d2367b9a0e1213b
Tomasz Zoladz <57683653+tom-zoladz@users.noreply.github.com>
2023-11-12 11:13:00 +0100

    GetPlantEntityViewEndpoint - Response<PlantEntityView> used for return type (#51)

--- src/main/java/dev/enginecode/weeds/plantcareservice/plantentity/presentation/handlers/GetPlantEntityViewQueryHandler.java
+++ src/main/java/dev/enginecode/weeds/plantcareservice/plantentity/presentation/handlers/GetPlantEntityViewQueryHandler.java
@@ -1,13 +1,16 @@
 package dev.enginecode.weeds.plantcareservice.plantentity.presentation.handlers;
 
 import dev.enginecode.eccommons.cqrs.query.QueryHandler;
+import dev.enginecode.eccommons.structures.model.Response;
 import dev.enginecode.weeds.plantcareservice.plantentity.presentation.model.PlantEntityView;
 import dev.enginecode.weeds.plantcareservice.plantentity.presentation.ports.GetPlantEntityViewPort;
 import dev.enginecode.weeds.plantcareservice.plantentity.presentation.queries.GetPlantEntityViewQuery;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
-public class GetPlantEntityViewQueryHandler implements QueryHandler<PlantEntityView, GetPlantEntityViewQuery> {
+public class GetPlantEntityViewQueryHandler implements QueryHandler<Response<PlantEntityView>, GetPlantEntityViewQuery> {
     GetPlantEntityViewPort port;
 
     GetPlantEntityViewQueryHandler(GetPlantEntityViewPort port) {
@@ -15,7 +18,10 @@ public class GetPlantEntityViewQueryHandler implements QueryHandler<PlantEntityV
     }
 
     @Override
-    public PlantEntityView handle(GetPlantEntityViewQuery query) {
-        return port.findById(query.id());
+    public Response<PlantEntityView> handle(GetPlantEntityViewQuery query) {
+        PlantEntityView plantEntityView = port.findById(query.id());
+        return new Response<>(
+                List.of( plantEntityView )
+        );
     }
 }
--- src/main/java/dev/enginecode/weeds/plantcareservice/plantentity/presentation/queries/GetPlantEntityViewQuery.java
+++ src/main/java/dev/enginecode/weeds/plantcareservice/plantentity/presentation/queries/GetPlantEntityViewQuery.java
@@ -1,13 +1,14 @@
 package dev.enginecode.weeds.plantcareservice.plantentity.presentation.queries;
 
 import dev.enginecode.eccommons.cqrs.query.Query;
+import dev.enginecode.eccommons.structures.model.Response;
 import dev.enginecode.weeds.plantcareservice.plantentity.presentation.model.PlantEntityView;
 
 import java.util.UUID;
 
-public record GetPlantEntityViewQuery(UUID id, String name) implements Query<PlantEntityView> {
+public record GetPlantEntityViewQuery(UUID id, String name) implements Query<Response<PlantEntityView>> {
 
-    public static GetPlantEntityViewQuery fromId(UUID id) {
+    public static GetPlantEntityViewQuery byId(UUID id) {
         return new GetPlantEntityViewQuery(id, null);
     }
 }
